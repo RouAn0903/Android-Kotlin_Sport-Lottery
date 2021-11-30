6f9b9517423789121e0819ce2c78657df040efe6
RouAn <409570183@m365.fju.edu.tw>
2021-11-30 22:31:53 +0800

    start and finish buttons

--- app/src/main/java/com/team1/sportlottery/SecActivity.kt
+++ app/src/main/java/com/team1/sportlottery/SecActivity.kt
@@ -1,5 +1,6 @@
 package com.team1.sportlottery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -8,6 +9,7 @@ import android.os.Message
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import java.util.zip.Adler32
 
 class SecActivity : AppCompatActivity() {
@@ -15,37 +17,70 @@ class SecActivity : AppCompatActivity() {
     private var progressCar1 =0
     private var progressCar2 =0
     private var progressCar3 =0
+    private var progressCar4 =0
+    private var progressCar5 =0
 
     private lateinit var btn_start: Button
+    private lateinit var btn_again: Button
+    private lateinit var btn_finish: Button
     private lateinit var car1: SeekBar
     private lateinit var car2: SeekBar
     private lateinit var car3: SeekBar
+    private lateinit var car4: SeekBar
+    private lateinit var car5: SeekBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sec)
 
         btn_start = findViewById(R.id.btn_start)
+        btn_again = findViewById(R.id.btn_again)
+        btn_finish = findViewById(R.id.btn_finish)
         car1 = findViewById(R.id.car1)
         car2 = findViewById(R.id.car2)
         car3 = findViewById(R.id.car3)
+        car4 = findViewById(R.id.car4)
+        car5 = findViewById(R.id.car5)
 
         btn_start.setOnClickListener{
             btn_start.isEnabled = false
             progressCar1 = 0
             progressCar2 = 0
             progressCar3 = 0
+            progressCar4 = 0
+            progressCar5 = 0
 
             car1.progress = 0
             car2.progress = 0
             car3.progress = 0
+            car4.progress = 0
+            car5.progress = 0
 
             runCar1()
             runCar2()
             runCar3()
+            runCar4()
+            runCar5()
+        }
+
+        btn_again.setOnClickListener {
+
+        }
+
+        btn_finish.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("結束")
+                .setMessage("確定要離開嗎?")
+                .setNeutralButton("確定"){ dialog, which ->
+                   startActivity(Intent(this, MainActivity::class.java))
+                }
+                .setPositiveButton("返回"){ dialog, which ->
+                   showToast("返回")
+                }.show()
         }
 
     }
+
 
     private fun showToast(msg: String) =
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
@@ -59,27 +94,41 @@ class SecActivity : AppCompatActivity() {
             car2.progress = progressCar2
         else if (msg.what == 3)
             car3.progress = progressCar3
+        else if (msg.what == 4)
+            car4.progress = progressCar4
+        else if (msg.what == 5)
+            car5.progress = progressCar5
         //若兔子抵達，則顯示兔子勝利
-        if (progressCar1 >= 100 && progressCar2 < 100 && progressCar3 < 100) {
+        if (progressCar1 >= 100 && progressCar2 < 100 && progressCar3 < 100 && progressCar4 < 100 && progressCar5 < 100) {
             showToast("Car1勝利") //顯示Car1勝利
             btn_start.isEnabled = true //按鈕可操作
         }
-        else if (progressCar2 >= 100 && progressCar1 < 100 && progressCar3 < 100) {
+        else if (progressCar2 >= 100 && progressCar1 < 100 && progressCar3 < 100 && progressCar4 < 100 && progressCar5 < 100) {
             showToast("Car2勝利") //顯示Car2勝利
             btn_start.isEnabled = true //按鈕可操作
         }
-        else if (progressCar3 >= 100 && progressCar1 < 100 && progressCar2 < 100){
+        else if (progressCar3 >= 100 && progressCar1 < 100 && progressCar2 < 100 && progressCar4 < 100 && progressCar5 < 100){
             showToast("Car3勝利") //顯示Car3勝利
             btn_start.isEnabled = true //按鈕可操作
         }
+        else if (progressCar4 >= 100 && progressCar1 < 100 && progressCar2 < 100 && progressCar3 < 100 && progressCar5 < 100){
+            showToast("Car4勝利") //顯示Car3勝利
+            btn_start.isEnabled = true //按鈕可操作
+        }
+        else if (progressCar5 >= 100 && progressCar1 < 100 && progressCar2 < 100 && progressCar3 < 100 && progressCar4 < 100){
+            showToast("Car5勝利") //顯示Car3勝利
+            btn_start.isEnabled = true //按鈕可操作
+        }
         true
+
+
     }
 
     private fun runCar1(){
         Thread {
             //兔子有三分之二的機率會偷懶
             val sleepProbability = arrayOf(true, true, false)
-            while (progressCar1 < 100 && progressCar2 < 100 && progressCar3 < 100) {
+            while (progressCar1 < 100 && progressCar2 < 100 && progressCar3 < 100 && progressCar4 < 100 && progressCar5 < 100) {
                 try {
                     Thread.sleep(100) //延遲 0.1 秒更新賽況
                     if (sleepProbability.random())
@@ -100,7 +149,7 @@ class SecActivity : AppCompatActivity() {
         Thread {
             //兔子有三分之二的機率會偷懶
             val sleepProbability = arrayOf(true, true, false)
-            while (progressCar1 < 100 && progressCar2 < 100 && progressCar3 < 100) {
+            while (progressCar1 < 100 && progressCar2 < 100 && progressCar3 < 100 && progressCar4 < 100 && progressCar5 < 100) {
                 try {
                     Thread.sleep(100) //延遲 0.1 秒更新賽況
                     if (sleepProbability.random())
@@ -121,7 +170,7 @@ class SecActivity : AppCompatActivity() {
         Thread {
             //兔子有三分之二的機率會偷懶
             val sleepProbability = arrayOf(true, true, false)
-            while (progressCar1 < 100 && progressCar2 < 100 && progressCar3 < 100) {
+            while (progressCar1 < 100 && progressCar2 < 100 && progressCar3 < 100 && progressCar4 < 100 && progressCar5 < 100) {
                 try {
                     Thread.sleep(100) //延遲 0.1 秒更新賽況
                     if (sleepProbability.random())
@@ -135,5 +184,47 @@ class SecActivity : AppCompatActivity() {
                 handler.sendMessage(msg) //傳送兔子的賽況訊息
             }
         }.start() //啟動 Thread
+    }
+
+    private fun runCar4(){
+        Thread {
+            //兔子有三分之二的機率會偷懶
+            val sleepProbability = arrayOf(true, true, false)
+            while (progressCar1 < 100 && progressCar2 < 100 && progressCar3 < 100 && progressCar4 < 100 && progressCar5 < 100) {
+                try {
+                    Thread.sleep(100) //延遲 0.1 秒更新賽況
+                    if (sleepProbability.random())
+                        Thread.sleep(300) //兔子偷懶 0.3 秒
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+                progressCar4 += 3 //每次跑三步
+                val msg = Message() //建立 Message 物件
+                msg.what = 4 //加入編號
+                handler.sendMessage(msg) //傳送兔子的賽況訊息
+            }
+        }.start() //啟動 Thread
+
+    }
+
+    private fun runCar5(){
+        Thread {
+            //兔子有三分之二的機率會偷懶
+            val sleepProbability = arrayOf(true, true, false)
+            while (progressCar1 < 100 && progressCar2 < 100 && progressCar3 < 100 && progressCar4 < 100 && progressCar5 < 100) {
+                try {
+                    Thread.sleep(100) //延遲 0.1 秒更新賽況
+                    if (sleepProbability.random())
+                        Thread.sleep(300) //兔子偷懶 0.3 秒
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+                progressCar5 += 3 //每次跑三步
+                val msg = Message() //建立 Message 物件
+                msg.what = 5 //加入編號
+                handler.sendMessage(msg) //傳送兔子的賽況訊息
+            }
+        }.start() //啟動 Thread
+
     }
 }
